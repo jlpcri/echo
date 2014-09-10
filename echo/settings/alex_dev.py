a381287bf13af18933f314e221e0772b242ac9db
Alex Heyden <caheyden@west.com>
2014-09-10 15:45:44 -0500

    Updated requirements, tweaked to make run on my local machine.

---
+++ echo/settings/alex_dev.py
@@ -0,0 +1,3 @@
+from base import *
+
+STATIC_URL = 'http://apps.qaci01.wic.west.com/static/'
--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import socket
 
 if __name__ == "__main__":
+    if socket.gethostname() == "QAIMint":
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "echo.settings.alex_dev")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "echo.settings.base")
 
     from django.core.management import execute_from_command_line
