58768f4a6cf279a0d870f65f1b38ec69887ecc44
Eric Whitcomb <EWhitcomb@west.com>
2014-10-07 11:39:31 -0500

    Removed code used in debugging datetime issue.

--- echo/apps/projects/helpers.py
+++ echo/apps/projects/helpers.py
@@ -81,12 +81,6 @@ def fetch_slots_from_server(project, sftp):
                     bravo_time = datetime.fromtimestamp(fs.mtime)
                     if slot.bravo_time:
                         bravo_time = slot.bravo_time.replace(tzinfo=None)
-                    bt = "bravo_time= {0}".format(type(bravo_time))
-                    sbt = "slot.bravo_time= {0}".format(type(slot.bravo_time))
-                    fsmtime = "fs.mtime= {0}".format(type(datetime.fromtimestamp(fs.mtime)))
-                    print bt
-                    print sbt
-                    print fsmtime
                     if slot.status == VoiceSlot.MISSING:
                         slot.status = VoiceSlot.NEW
                         slot.bravo_checksum = fs.msum
