e69c234b6491cfafafdbcea49b120c62e8c7c534
Alex Heyden <caheyden@west.com>
2014-10-13 15:41:57 -0500

    UI tweaks and unit testing

--- echo/apps/elpis/tests.py
+++ echo/apps/elpis/tests.py
@@ -1,35 +1,84 @@
+import json
+
 from django import test
 from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
 
 from echo.apps.projects.models import Project
+from echo.apps.settings.models import Server, PreprodServer
 
 User = get_user_model()
+
+class TestDashboardView(test.TestCase):
+    def setUp(self):
+        self.client = test.Client()
+        User.objects.create_user(username='test_user', password='test')
+        self.client.login(username='test_user', password='test')
+        self.url = reverse('elpis:dashboard', args=(1,))
+        Project.objects.create(name='Test Project')
+
+    def test_dashboard_loads(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
 
 class TestSetServerView(test.TestCase):
     def setUp(self):
         self.client = test.Client()
         User.objects.create_user(username='test_user', password='test')
-        Project.objects.create(name='Test Project',)
+
+        Server.objects.create(name='Fake Bravo')
+        # TODO: Mock preprod servers
+        PreprodServer.objects.create(name='linux4095', address='linux4095.wic.west.com', account='wicqacip',
+                                     application_type=PreprodServer.PRODUCER)
+        PreprodServer.objects.create(name='Prompt Store Placeholder', application_type=PreprodServer.NATIVE_VXML)
+        Project.objects.create(name='Test Project', bravo_server=Server.objects.first())
         self.client.login(username='test_user', password='test')
         self.url = reverse('elpis:set_server', args=(1,))
 
-    def test_dashboard_load(self):
-        response = self.client.get(self.url)
+    def test_set_producer(self):
+        """Test that the Producer server returns success and a list of paths"""
+        producer_pk = PreprodServer.objects.get(application_type=PreprodServer.PRODUCER).pk
+        response = self.client.post(self.url, {'preprod-server': producer_pk})
         self.assertEqual(response.status_code, 200)
+        response_body = json.loads(response.content)
+        self.assertTrue(response_body['success'])
+        self.assertTrue(response_body['paths'])
+        self.assertEqual(Project.objects.first().preprod_server.pk, producer_pk)
 
-    def test_matching_confirm_screen(self):
-        response = self.client.post(self.url, {})
-        self.assertTrue(False)
+class TestSetPathView(test.TestCase):
+    def setUp(self):
+        self.client = test.Client()
+        User.objects.create_user(username='test_user', password='test')
 
-    def test_match_disconfirm(self):
-        self.assertTrue(False)
+        Server.objects.create(name='Fake Bravo')
+        # TODO: Mock preprod servers
+        PreprodServer.objects.create(name='linux4095', address='linux4095.wic.west.com', account='wicqacip',
+                                     application_type=PreprodServer.PRODUCER)
+        PreprodServer.objects.create(name='Prompt Store Placeholder', application_type=PreprodServer.NATIVE_VXML)
+        Project.objects.create(name='Test Project', bravo_server=Server.objects.first())
+        self.client.login(username='test_user', password='test')
+        self.set_server_url = reverse('elpis:set_server', args=(1,))
+        self.set_path_url = reverse('elpis:set_path', args=(1,))
 
-    def test_match_confirmed(self):
-        self.assertTrue(False)
+    def test_set_producer_path(self):
+        """Fetch paths for the producer server, then set one"""
+        producer_pk = PreprodServer.objects.get(application_type=PreprodServer.PRODUCER).pk
+        response = self.client.post(self.set_server_url, {'preprod-server': producer_pk})
+        self.assertEqual(response.status_code, 200)
+        response_body = json.loads(response.content)
+        self.assertTrue(response_body['success'])
+        self.assertTrue(response_body['paths'])
 
-    def test_project_select_screen(self):
-        self.assertTrue(False)
+        path = response_body['paths'][0]
+        response = self.client.post(self.set_path_url, {'preprod-path': path})
+        self.assertEqual(response.status_code, 200)
+        response_body = json.loads(response.content)
+        self.assertTrue(response_body['success'])
+        self.assertEqual(response_body['path'], '/usr/local/tuvox/public/Projects/' + path)
+        self.assertEqual(response_body['path'], Project.objects.first().preprod_path)
+
+
 
 class TestVerifyView(test.TestCase):
     def setUp(self):
--- echo/apps/elpis/views.py
+++ echo/apps/elpis/views.py
@@ -1,5 +1,7 @@
 import json
 
+from mock import MagicMock
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
