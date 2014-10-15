5cae9d5ed57afd890fecdc1537360c86f9d30c67
Songqing Liu <sliu@west.com>
2014-10-15 09:55:29 -0500

    Add preprod servers to Settings page

--- echo/apps/settings/views.py
+++ echo/apps/settings/views.py
@@ -111,3 +111,66 @@ def users(request):
             messages.danger(request, "Unable to delete user")
             return render(request, "settings/users.html", {'users': User.objects.all().order_by("username")})
     return HttpResponseNotFound()
+
+
+@user_passes_test(user_is_superuser)
+def servers_preprod(request):
+    if request.method == 'GET':
+        return render(request, "settings/servers_preprod.html",
+                      {'servers': Server.objects.all().order_by("name"), 'server_form': ServerForm()})
+    elif request.method == 'POST':
+        if "add_server" in request.POST:
+            form = ServerForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                address = form.cleaned_data['address']
+                account = form.cleaned_data['account']
+                server = Server(name=name, address=address, account=account)
+                try:
+                    server.full_clean()
+                    server.save()
+                    messages.success(request, "Added server to list")
+                    return redirect("settings:servers_preprod")
+                except ValidationError as e:
+                    if 'name' in e.message_dict:
+                        messages.danger(request, e.message_dict.get('name')[0])
+                    return render(request, "settings/servers_preprod.html",
+                                  {'servers': Server.objects.all().order_by("name"), 'server_form': form})
+            messages.danger(request, "Unable to add server to list")
+            return render(request, "settings/servers_preprod.html",
+                          {'servers': Server.objects.all().order_by("name"), 'server_form': form})
+        elif "delete_server" in request.POST:
+            sid = request.POST.get('sid', "")
+            if sid:
+                server = get_object_or_404(Server, pk=sid)
+                server.delete()
+                messages.success(request, "Server \"{0}\" has been deleted".format(server.name))
+                return redirect("settings:servers_preprod")
+            messages.danger(request, "Unable to delete server")
+            return render(request, "settings/servers_preprod.html",
+                          {'servers': Server.objects.all().order_by("name"), 'server_form': ServerForm()})
+        elif "test_connection" in request.POST:
+            sid = request.POST.get('sid', "")
+            if sid:
+                server = get_object_or_404(Server, pk=sid)
+                try:
+                    with pysftp.Connection(server.address, username=str(server.account)) as conn:
+                        conn.chdir('/')
+                except IOError:
+                    messages.danger(request, "Unable to connect to server \"{0}\"".format(server.name))
+                    return redirect("settings:servers_preprod")
+                except pysftp.ConnectionException:
+                    messages.danger(request, "Connection error to server \"{0}\"".format(server.name))
+                    return redirect("settings:servers_preprod")
+                except pysftp.CredentialException:
+                    messages.danger(request, "Credentials error to server \"{0}\"".format(server.name))
+                    return redirect("settings:servers_preprod")
+                except pysftp.AuthenticationException:
+                    messages.danger(request, "Authentication error to server \"{0}\"".format(server.name))
+                    return redirect("settings:servers_preprod")
+                except pysftp.SSHException:
+                    messages.danger(request, "SSH error to server \"{0}\"".format(server.name))
+                    return redirect("settings:servers_preprod")
+                messages.success(request, "Connecting to server \"{0}\"".format(server.name))
+                return redirect("settings:servers_preprod")
+    return HttpResponseNotFound()
