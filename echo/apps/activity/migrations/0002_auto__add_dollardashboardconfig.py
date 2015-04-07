817cbeac4d9d24a75c6323fc61e8b72e17c03d40
Songqing Liu <sliu@west.com>
2015-04-07 11:29:53 -0500

    Add migrations to apps/activity

---
+++ echo/apps/activity/migrations/0002_auto__add_dollardashboardconfig.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'DollarDashboardConfig'
+        db.create_table(u'activity_dollardashboardconfig', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('tester_pass_slot', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=12, decimal_places=2)),
+            ('tester_fail_slot', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=12, decimal_places=2)),
+            ('auto_new_slot', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=12, decimal_places=2)),
+            ('auto_pass_slot', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=12, decimal_places=2)),
+            ('auto_fail_slot', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=12, decimal_places=2)),
+            ('update_file_status', self.gf('django.db.models.fields.DecimalField')(default=0, max_digits=12, decimal_places=2)),
+            ('elasticsearch_url', self.gf('django.db.models.fields.CharField')(default='', max_length=100)),
+            ('elasticsearch_index', self.gf('django.db.models.fields.CharField')(default='', max_length=40)),
+        ))
+        db.send_create_signal(u'activity', ['DollarDashboardConfig'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'DollarDashboardConfig'
+        db.delete_table(u'activity_dollardashboardconfig')
+
+
+    models = {
+        u'activity.action': {
+            'Meta': {'object_name': 'Action'},
+            'actor': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'scope': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['activity.Scope']"}),
+            'time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'type': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+        },
+        u'activity.dollardashboardconfig': {
+            'Meta': {'object_name': 'DollarDashboardConfig'},
+            'auto_fail_slot': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '12', 'decimal_places': '2'}),
+            'auto_new_slot': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '12', 'decimal_places': '2'}),
+            'auto_pass_slot': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '12', 'decimal_places': '2'}),
+            'elasticsearch_index': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '40'}),
+            'elasticsearch_url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tester_fail_slot': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '12', 'decimal_places': '2'}),
+            'tester_pass_slot': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '12', 'decimal_places': '2'}),
+            'update_file_status': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '12', 'decimal_places': '2'})
+        },
+        u'activity.scope': {
+            'Meta': {'object_name': 'Scope'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Language']", 'null': 'True', 'blank': 'True'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']", 'null': 'True', 'blank': 'True'}),
+            'scope': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'voiceslot': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.VoiceSlot']", 'null': 'True', 'blank': 'True'})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'projects.language': {
+            'Meta': {'object_name': 'Language'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']"})
+        },
+        u'projects.project': {
+            'Meta': {'object_name': 'Project'},
+            'bravo_server': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['settings.Server']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'failure_count': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {'unique': 'True'}),
+            'preprod_path': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'preprod_server': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['settings.PreprodServer']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'root_path': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.TextField', [], {'default': "'Initial'"}),
+            'tests_run': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.User']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'projects.voiceslot': {
+            'Meta': {'object_name': 'VoiceSlot'},
+            'bravo_checksum': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'bravo_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'check_in_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'checked_out': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'checked_out_time': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Language']"}),
+            'name': ('django.db.models.fields.TextField', [], {}),
+            'path': ('django.db.models.fields.TextField', [], {}),
+            'status': ('django.db.models.fields.TextField', [], {'default': "'New'"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'verbiage': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'vuid': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.VUID']", 'null': 'True', 'on_delete': 'models.SET_NULL'}),
+            'vuid_time': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'projects.vuid': {
+            'Meta': {'object_name': 'VUID'},
+            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'filename': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['projects.Project']"}),
+            'upload_by': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'on_delete': 'models.SET_NULL'}),
+            'upload_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        u'settings.preprodserver': {
+            'Meta': {'object_name': 'PreprodServer'},
+            'account': ('django.db.models.fields.TextField', [], {}),
+            'address': ('django.db.models.fields.TextField', [], {'unique': 'True'}),
+            'application_type': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {'unique': 'True'})
+        },
+        u'settings.server': {
+            'Meta': {'object_name': 'Server'},
+            'account': ('django.db.models.fields.TextField', [], {}),
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'address': ('django.db.models.fields.TextField', [], {'unique': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {'unique': 'True'})
+        }
+    }
+
+    complete_apps = ['activity']
